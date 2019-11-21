9cc245f195b8bc881501a8f6324a1539dff4acb3
JAZIEL LOPEZ <jaziel.lopez@thermofisher.com>
2019-11-21 13:00:09 -0800

    localstack | test previous installation

--- localstack-start.sh
+++ localstack-start.sh
@@ -9,8 +9,38 @@
 #              https://github.com/localstack/localstack
 # Support: OSX as is; for linux do not overwrite TMPDIR; ignore line 19
 
+if [ -z $LOCALSTACK_INSTALLATION_PATH ]; then
 
-export LOCALSTACK_INSTALLATION_PATH=;  # enter path where was localstack cloned in
+    HERE=$PWD/localstack
+    
+    if [ ! -d $HERE ]; then
+        mkdir $HERE
+        echo "[INFO] localstack will be cloned at $HERE ..."
+        git clone https://github.com/localstack/localstack $HERE
+    else
+        echo "[INFO] localstack already cloned at $HERE"
+    fi
+
+    if [ $? == 0 ]; then
+        
+        if [[ $SHELL == *"bash"* ]]; then
+            echo "export LOCALSTACK_INSTALLATION_PATH=$HERE" >> $HOME/.bashrc
+            export LOCALSTACK_INSTALLATION_PATH=$HERE
+            echo "[INFO] Written LOCALSTACK_INSTALLATION_PATH to $HOME/.bashrc"
+        fi
+        
+        if [[ $SHELL == *"zsh"* ]]; then
+            echo "export LOCALSTACK_INSTALLATION_PATH=$HERE" >> $HOME/.zshrc
+            export LOCALSTACK_INSTALLATION_PATH=$HERE
+            echo "[INFO] Written LOCALSTACK_INSTALLATION_PATH to $HOME/.zshrc"
+        fi
+    else
+        echo "[ERROR] localstack git clone failed with errors"
+        exit 1
+    fi
+fi
+
+# export LOCALSTACK_INSTALLATION_PATH=;  # enter path where was localstack cloned in
 
 echo "=============================================="
 echo "[INFO] starting localstack services...."
