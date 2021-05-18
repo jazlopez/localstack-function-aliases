f75c77717c924512a3e5e192fe76e0a930602310
Jaziel Lopez <juan.jaziel@gmail.com>
2021-05-18 01:44:12 -0700

    autosymlink to zsh custom

--- localstack.function.aliases.zsh
+++ localstack.function.aliases.zsh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+if [[ ! -v ZSH_CUSTOM ]]; then
+    printf "[ERROR] %s\n" "ZSH_CUSTOM environment variable could not be found: is ZSH installed on your system?"
+    exit 1
+fi
+
 # CONFIGURE REQUIRED VARIABLE ENVIRONMENT
-export AWS_ACCESS_KEY_ID="test" 
-export AWS_SECRET_ACCESS_KEY="test"
-export AWS_DEFAULT_REGION="us-east-1"
+# export AWS dummy values (if not already set)
+if [ -z "${AWS_ACCESS_KEY_ID}" ]; then
+    export AWS_ACCESS_KEY_ID="test" 
+fi
+if [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then
+    export AWS_SECRET_ACCESS_KEY="test" 
+fi
+
+if [ -z "${AWS_DFAULT_REGION}" ]; then
+    export AWS_DEFAULT_REGION="us-east-1" 
+fi
+
+
 export LOCALSTACKHOST="http://localhost"
 export LOCALSTACKPORT=4566
 export LOCALSTACKURL="${LOCALSTACKHOST}:${LOCALSTACKPORT}"
@@ -17,6 +32,9 @@ function _contactme_(){
   printf "  %s\n" $CONTACTME
   printf "  %s\n" $CONTACTURL
 }
+
+printf "%s\n" "creating symlinks for aliases under $ZSH_CUSTOM"
+ln -s s3.alias.sh $ZSH_CUSTOM/localstack.s3.aliases.zsh
 
 LOCALSTACK_S3_ALIAS_FILE="$PWD/s3.alias.sh"
 
